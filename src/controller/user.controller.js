2de171f378c146d6f2f6498db4960801c909de80
Hugo Costa <informig@gmail.com>
2025-12-05 16:29:34 -0300

    Fix: fix do exercicio 1 e entrega do exercicio 2

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -36,7 +36,7 @@ const userLogin = async (req, res) => {
 
     // A propriedade expiresIn aceita o tempo de forma bem descritiva. Por exemplo: '7d' = 7 dias.
     const jwtConfig = {
-      expiresIn: '7d',
+      expiresIn: '1h',
       algorithm: 'HS256',
     };
 
@@ -47,7 +47,14 @@ const userLogin = async (req, res) => {
       eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
       .eyJkYXRhIjp7ImlkIjozLCJ1c2VybmFtZSI6Iml0YWxzc29kaiIsInBhc3N3b3JkIjoic2VuaGExMjMifSwiaWF0IjoxNjM4OTc1MTMyLCJleHAiOjE2Mzk1Nzk5MzJ9
       .hnpmu2p61Il8wdQfmUiJ7wiWXgw8UuioOU_D2RnB9kY */
-    const token = jwt.sign({ data: { userId: user.data.id }}, secret, jwtConfig);
+    let token = '';
+
+    if (user.data.userName === 'admin' && user.data.password === 's3nh4S3gur4???') {
+        token = jwt.sign({ data: { userId: user.data.id, name: user.data.userName, admin: true }}, secret, jwtConfig);
+    } else {
+        token = jwt.sign({ data: { userId: user.data.id, name: user.data.userName, admin: false }}, secret, jwtConfig);
+    }
+    
 
     /* Por fim, nós devolvemos essa informação ao usuário. */
     res.status(mapStatusHTTP('SUCCESSFUL')).json({ token });

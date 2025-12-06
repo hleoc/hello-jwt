7ea6da277d6372578d21968925918a5df6841c4b
Hugo Costa <informig@gmail.com>
2025-12-06 09:12:18 -0300

    Feat: Exercicio 4 - /GET /top-secret

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 const route = require('express').Router();
 const { userController } = require('../controller');
 const validateJWT = require('../auth/validateJWT');
+const validateAdmin = require('../middlewares/admin');
 
 route.post('/users', userController.createUsers);
 route.post('/login', userController.userLogin);
 route.get('/users/me', validateJWT, userController.findByMe);
+route.get('/top-secret', validateJWT, validateAdmin, userController.findSecret);
 
 module.exports = route;
